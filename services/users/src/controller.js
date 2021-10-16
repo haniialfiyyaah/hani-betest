fcd8c17d902e075c29e5f6b75f55be6994fcc248
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-10-17 02:17:12 +0700

    add crud user

--- services/users/src/controller.js
+++ services/users/src/controller.js
@@ -1,6 +1,79 @@
+const User = require('./model')
 class UserController {
   static async index(req, res, next) {
-    res.status(200).json({ message: 'ok' })
+    try {
+      let users = await User.find()
+      res.status(200).json({ message: 'users found.', data: users })
+    } catch (err) {
+      console.log(err)
+      let message = err?.message || 'Internal Server Error'
+      res.status(500).json({ message: message, errors: err?.errors })
+    }
+  }
+
+  static async store(req, res) {
+    try {
+      const { userName, accountNumber, emailAddress, identityNumber } = req.body
+      const newUser = await User.create({
+        userName,
+        accountNumber,
+        emailAddress,
+        identityNumber,
+      })
+      res.status(200).json({ message: 'user created.', data: newUser })
+    } catch (err) {
+      console.log(err)
+      let message = err?.message || 'Internal Server Error'
+      res.status(500).json({ message: message, errors: err?.errors })
+    }
+  }
+
+  static async show(req, res) {
+    try {
+      const { id } = req.params
+      let user = await User.findOne({ $text: { $search: id } })
+
+      if (!user) return res.status(404).json({ message: 'User not found' })
+      res.status(200).json({ message: 'user found', data: user })
+    } catch (err) {
+      console.log(err)
+      let message = err?.message || 'Internal Server Error'
+      res.status(500).json({ message: message, errors: err?.errors })
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      const { id } = req.params
+      const { userName, accountNumber, emailAddress, identityNumber } = req.body
+
+      let user = await User.findByIdAndUpdate(id, {
+        userName,
+        accountNumber,
+        emailAddress,
+        identityNumber,
+      })
+
+      if (!user) return res.status(404).json({ message: 'User not found' })
+      res.status(200).json({ message: 'user updated' })
+    } catch (err) {
+      console.log(err)
+      let message = err?.message || 'Internal Server Error'
+      res.status(500).json({ message: message, errors: err?.errors })
+    }
+  }
+
+  static async destroy(req, res, next) {
+    try {
+      const { id } = req.params
+      let user = await User.findByIdAndDelete(id)
+      if (!user) return res.status(404).json({ message: 'User not found' })
+      res.status(200).json({ message: 'user deleted' })
+    } catch (err) {
+      console.log(err)
+      let message = err?.message || 'Internal Server Error'
+      res.status(500).json({ message: message, errors: err?.errors })
+    }
   }
 }
 
--- services/users/src/router.js
+++ services/users/src/router.js
@@ -2,5 +2,9 @@ const router = require('express').Router()
 const { UserController } = require('./controller')
 
 router.get('/', UserController.index)
+router.get('/:id', UserController.show)
+router.post('/', UserController.store)
+router.put('/:id', UserController.update)
+router.delete('/:id', UserController.destroy)
 
 module.exports = router
