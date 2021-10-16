ff098f058aae546d0f40955aa031714c59ee75df
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-10-17 02:15:57 +0700

    update config mongoose

--- services/users/src/index.js
+++ services/users/src/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./router')
-const { db } = require('./config')
+require('./config') // mongoose config
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -13,10 +13,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use(router)
-
-db.once('open', () => {
-  console.log('Connected successfully')
-})
 
 app.listen(PORT, () => {
   console.log(`Listening to user services on port ${PORT} 🎶`)
