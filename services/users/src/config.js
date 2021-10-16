0db0f6b68cb72464ba14299189cb33439d071ad6
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-10-16 23:26:27 +0700

    Config mongoose

---
+++ services/users/src/config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+//Set up default mongoose connection
+const MONGO_URI = process.env.MONGO_URI
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+module.exports = { db }
--- services/users/src/index.js
+++ services/users/src/index.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV === 'development') require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const router = require('./router')
+const { db } = require('./config')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -9,8 +12,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
-// router
 app.use(router)
+
+db.once('open', () => {
+  console.log('Connected successfully')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening to user services on port ${PORT} 🎶`)
