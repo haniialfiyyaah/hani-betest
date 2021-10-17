802d11c037dc5e5e5f9320defca47daaa49d84c7
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-10-17 16:09:29 +0700

    fix bug seeding

--- services/users/src/seed.js
+++ services/users/src/seed.js
@@ -1,18 +1,27 @@
-const { Auth } = require('./model')
+require('dotenv').config()
 
+const { Auth } = require('./model')
 // seeding
 const fs = require('fs/promises')
 const mongoose = require('mongoose')
+const { hashPassword } = require('./helpers/bcrypt')
+const MONGO_URI = process.env.MONGO_URI
+
+// connect to databse once to seed
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected for seeding'))
+  .catch((err) => console.log(err))
+
 const seedDB = async () => {
   const data = JSON.parse((await fs.readFile('auth.json')).toString())
-  const auth = await Auth.find()
-  if (auth?.length > 0) await Auth.deleteMany({})
-  data.forEach(async (e) => {
-    await Auth.create(e)
-  })
+  data.map(async (e) => (e.password = hashPassword(e.password)))
+  await Auth.deleteMany({})
+  await Auth.insertMany(data)
 }
 
 seedDB().then(() => {
   console.log('Auth user seed successfully')
+  // close database
   mongoose.connection.close()
 })
